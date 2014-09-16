8f657ee220c31e7c38e63e2753c49f36e30bcb1e
LatexBotox <Lowboll@gmail.com>
2014-09-16 05:30:31 +0200

    parallax bg
    stars galore

---
+++ Assets/Scripts/BgScroller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgScroller : MonoBehaviour {
+
+	public Rigidbody2D ship;
+	public float scroll_speed;
+
+	private Vector2 s_offset;
+
+	void Start () {
+		s_offset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
+	}
+
+	void Update () {
+
+		float scroll_x = Mathf.Repeat(ship.position.x * scroll_speed, 1);
+		float scroll_y = Mathf.Repeat(ship.position.y * scroll_speed, 1);
+		renderer.sharedMaterial.SetTextureOffset("_MainTex", new Vector2(scroll_x, scroll_y));
+
+
+		Vector3 pos = new Vector3(ship.position.x, ship.position.y, transform.position.z);
+		transform.position = pos;
+	}
+
+	void OnDisable() {
+		renderer.sharedMaterial.SetTextureOffset("_MainTex", s_offset);
+	}
+}
