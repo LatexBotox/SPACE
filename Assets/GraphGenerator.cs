35015066d84f11a18e1a56eb53179e7ef4402a58
darkvain <darkvain@gmail.com>
2014-10-04 18:55:02 +0200

    A little bit of generation optimazation, hopefully

--- Assets/GraphGenerator.cs
+++ Assets/GraphGenerator.cs
@@ -3,7 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 
 
+
 class NoSpaceException : System.Exception {}
+
+
+class NodeComp : IComparer<Collider2D> {
+	Vector2 point;
+
+	public NodeComp(Vector2 p) {
+		point = p;
+	}
+
+	public int Compare(Collider2D c1, Collider2D c2) {
+		if (((Vector2)c1.transform.position - point).sqrMagnitude <
+			((Vector2)c2.transform.position - point).sqrMagnitude)
+			return -1;
+		else if (((Vector2)c2.transform.position - point).sqrMagnitude <
+			((Vector2)c1.transform.position - point).sqrMagnitude)
+			return 1;
+		else
+			return 0;
+	}
+}
+
 
 public class GraphGenerator : MonoBehaviour {
 
@@ -16,6 +38,8 @@ public class GraphGenerator : MonoBehaviour {
 	public int maxNrNodes;
 	public float radius;
 
+	float ntime;
+
 
 	Stack buildNodes;
 	ArrayList allNodes;
@@ -25,7 +49,8 @@ public class GraphGenerator : MonoBehaviour {
 
 		buildNodes = new Stack ();
 		allNodes = new ArrayList ();
-
+		ntime = 0;
+			
 		DFRandomWalk ();
 	
 	}
@@ -59,8 +84,10 @@ public class GraphGenerator : MonoBehaviour {
 				buildNodes.Pop();
 			}
 		}
-
+	
+		print ("Alahuaackbaar");
 		print ("Finishied generating graph with " + allNodes.Count + " nodes");
+		print ("Time spent looking for neighbours: " + ntime);
 	}
 
 	void BuildEdges(GraphNode me, ArrayList others) {
@@ -73,25 +100,33 @@ public class GraphGenerator : MonoBehaviour {
 		}
 	}
 
-	ArrayList FindNeighbours(Vector2 p) {
-		ArrayList ret = new ArrayList ();
-		SortedList candidates = new SortedList ();
 
-		foreach (GraphNode gn in allNodes) {
+	ArrayList FindNeighbours(Vector2 p) {
+		float t = Time.time;
+		ArrayList ret = new ArrayList ();
+		List<Collider2D> results = new List<Collider2D>();
+
+		
+		results.AddRange(Physics2D.OverlapCircleAll(p, maxDist, 1 << 16)); 
+		results.Sort( new NodeComp(p) );
+
+		/*foreach (GraphNode gn in allNodes) {
 			float distsq = (p - (Vector2)gn.transform.position).sqrMagnitude;
 			if(distsq < maxDist*maxDist && gn.edges.Count < 4) {
 				candidates.Add(distsq, gn);
 			}
-		}
+		}*/
 
 		int count = 0;
-		foreach (GraphNode gn in candidates.Values) {
+		foreach (Collider2D gn in results) {
 			if(++count > 3)
 				break;
 
-			ret.Add(gn);
+			ret.Add(gn.GetComponent<GraphNode>());
 		}
 
+
+		print ("nr neighbours: " + ret.Count);
 		return ret;
 	}
 
@@ -123,20 +158,21 @@ public class GraphGenerator : MonoBehaviour {
 			return false;
 		}
 
+	
+		if (Physics2D.OverlapCircle (p, minDist, 1 << 16) != null) {
+			print ("Failed to create node at: " + p + "(too close to others)");
+			return false;
 
-		foreach (GraphNode gn in allNodes) {
+		}
+
+		/*foreach (GraphNode gn in allNodes) {
 			float dist = (p - (Vector2)gn.transform.position).sqrMagnitude;
 			if(dist < minDist*minDist) {
 				print ("Failed to create node at: " + p + "(too close to others)");
 				return false;
 			}
-		}
+		}*/
 
 		return true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
